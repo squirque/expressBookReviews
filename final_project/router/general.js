9baa103eb5f8f3fdbe19b386f393bb47253cac47
squirque <squirque@gmail.com>
2024-06-07 22:01:00 +0400

    2024-06-07 commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,23 +7,26 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn],null,4))
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+//  const author = req.params.author;
+//  let filtered_authors = books.filter((book) => book.author === author);
+//  return res.send(JSON.stringify(filtered_authors,null,4));
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
